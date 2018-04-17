77ffd4738eaf3b6bc2e192797a0b4c648332ca68
Eole <xavierus70@hotmail.com>
2018-04-17 02:14:24 +0200

    Homepage layout

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Twig\Environment;
+
+
+class HomeController
+{
+    /**
+     * @Route("/home")
+     *
+     * @param Environment $environment
+     * @return Response
+     */
+    public function __invoke(Environment $environment)
+    {
+        return new Response(
+            $environment->render('home.html.twig')
+        );
+    }
+}
